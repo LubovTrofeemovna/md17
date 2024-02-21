3ec6c879d074ffbb2ccfa089acfe901e2b535726
LuboveTrofeemovna <a.trubaev@icloud.com>
2024-02-21 16:49:07 +0300

    --

--- test2.py
+++ test2.py
@@ -1,5 +1,8 @@
 a = int(input("введите номер места "))
-if a >= 37: print("боковое")
-else: print("купе")
+if a >= 37:
+    print("боковое")
+else:
+    print("купе")
 if a % 2 == 1: print("нижнее")
-else: print("верхнее")
+else:
+    print("верхнее")
